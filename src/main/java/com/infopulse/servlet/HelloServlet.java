d67e11f3becb70c7fe4deb4df1ffa34681a17787
vlad <vladkuzma99@gmail.com>
2019-05-18 12:37:11 +0300

    add HttpSession

--- src/main/java/com/infopulse/servlet/HelloServlet.java
+++ src/main/java/com/infopulse/servlet/HelloServlet.java
@@ -1,9 +1,6 @@
 package com.infopulse.servlet;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -12,20 +9,27 @@ public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String formValue = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie: cookies) {
-                if ("user".equals(cookie.getName())) {
-                    formValue = cookie.getValue();
-                }
-            }
-        }
-        if(formValue == null) {
+          String formValue = null;
+//        Cookie[] cookies = request.getCookies();
+//        if (cookies != null) {
+//            for (Cookie cookie: cookies) {
+//                if ("user".equals(cookie.getName())) {
+//                    formValue = cookie.getValue();
+//                }
+//            }
+//        }
+//        if(formValue == null) {
+//            formValue = request.getParameter("username");
+//            Cookie cookie = new Cookie("user", formValue);
+//            cookie.setMaxAge(1000);
+//            response.addCookie(cookie);
+//        }
+        HttpSession hs = request.getSession(true);
+        formValue = (String) hs.getAttribute("user");
+        if (formValue == null) {
             formValue = request.getParameter("username");
-            Cookie cookie = new Cookie("user", formValue);
-            cookie.setMaxAge(1000);
-            response.addCookie(cookie);
+            //hs.setMaxInactiveInterval(100000);
+            hs.setAttribute("user", formValue);
         }
 
 
