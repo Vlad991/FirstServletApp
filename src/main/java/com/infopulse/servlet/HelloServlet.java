0df1521350ae03c0ae927df72e736c354f50e421
vlad <vladkuzma99@gmail.com>
2019-05-18 11:14:21 +0300

    Initial commit

---
+++ src/main/java/com/infopulse/servlet/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.infopulse.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
---
+++ src/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <form action="/hello" method="get">
+            <input type="text" name="username" value=""/>
+            <input type="submit" name="ok" value="send"/>
+        </form>
+    </body>
+</html>
